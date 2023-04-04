90e70be78fc9a193c0a2dc153737251c48d59265
sitrakak <shajaniri@gmail.com>
2023-04-04 19:12:56 +0300

    Création des clients du template

---
+++ src/main/java/jsf/Bean3.java
@@ -0,0 +1,34 @@
+package jsf;
+
+import java.util.ArrayList;
+import java.util.List;
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+
+@Named(value = "bean3")
+@RequestScoped
+public class Bean3 {
+
+    private int nombre;
+
+    public int getNombre() {
+        return nombre;
+    }
+
+    public String afficher() {
+        return "affichage__4?nb="+ nombre + "&amp;faces-redirect=true";
+    }
+    
+    public void setNombre(int nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Integer> getNombresSuivants() {
+        int nb = 5;
+        List<Integer> l = new ArrayList<>(nb);
+        for (int i = nombre; i < nombre + nb; i++) {
+            l.add(i);
+        }
+        return l;
+    }
+}
